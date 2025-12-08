d4d52ff84622f595f249233078165646fff3cd3a
mike-henry <mike.sparx@gmail.com>
2025-12-08 18:57:23 +0000

    added ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -12,10 +11,10 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   build: {
-    sourcemap: true
-  }
+    sourcemap: true,
+  },
 })
